3da1f8b09d4741e5e2d53b2015d8b8c97e4a2b3e
Sofia Rodrigues <sofia@algebraic.dev>
2024-04-09 15:02:19 -0300

    init: x.x

---
+++ Parse.lean
@@ -0,0 +1,8 @@
+/-!
+  # Parse.lean
+
+  This library is useful for defining finite-state machines to parse things fast
+  using finite automata.
+-/
+
+namespace Parse
---
+++ lakefile.lean
@@ -0,0 +1,8 @@
+import Lake
+open Lake DSL
+
+package «Parse» where
+  -- add package configuration options here
+
+lean_lib «Parse» where
+  -- add library configuration options here
